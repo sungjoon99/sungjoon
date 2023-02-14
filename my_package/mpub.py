fc26bcbc604f0fed4839822757f3ac4871a638f5
sungjoon <cs0126j@naver.com>
2023-02-13 20:18:43 -0800

    aa

--- my_package/mpub.py
+++ my_package/mpub.py
@@ -1,5 +1,21 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+class M_pub(Node):
+  def __init__(self):
+    super().__init__()
+
+
 def main():
-  pass
+  rclpy.init()
+  node = M_pub()
+  pub = node.create_publisher(String, 'messagepub' , 10)
+  msg = String()
+  msg.date = 'hello, world'
+  while True:
+    pub.publish(msg)
+  node.destroy_node()
 
 if __name__ == '__main__':
   main()
