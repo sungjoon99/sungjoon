be6e4ff4d0caa0dbd3a821c120bf28c764610ba9
sungjoon <cs0126j@naver.com>
2023-02-13 18:21:33 -0800

    ahead

---
+++ python_test/test.py
@@ -0,0 +1,35 @@
+
+class Test:
+
+  def __init__(self):
+    self.var = 10
+    self.var2 = 20
+
+  def aa(self):
+    print('aaabb')
+
+
+  class Inh(Test):
+    super().__init__()
+
+def main():
+  a = 10
+  a = 'hellow'
+  b = int()
+  print(a)
+  ob =Test()
+  ob2 =Test()
+  inob = Inh
+  ob.aa()
+  ob.aa()
+
+
+
+  if __name__ == "__main__":
+    main()
+
+a = 10
+
+printf(a)
+
+printf(a)
