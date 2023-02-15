ff37195b266a5b97832f45740a412603519d0765
sungjoon <cs0126j@naver.com>
2023-02-15 00:51:26 -0800

    a

--- my_package/move_turtle.py
+++ my_package/move_turtle.py
@@ -7,34 +7,25 @@ class M_turtle(Node):
   def __init__(self):
     super().__init__('move_turtle')
     self.qos = QoSProfile(depth = 10)
-    self.pub1 = self.create_publisher(Twist, 'turtle1/cmd_vel', self.qos)
-    self.pub2 = self.create_publisher(Twist, 'turtle2/cmd_vel', self.qos)
+    self.pub = self.create_publisher(Twist, 'turtle1/cmd_vel', self.qos)
     self.create_timer(0.1, self.pubmessage)
     self.vel = 0.0
 
   def pubmessage(self):
     msg = Twist()
-    msg.linear.x = self.vel
+    msg.linear.x = 5.0
     msg.linear.y = 0.0
     msg.linear.z = 0.0
+
     msg.angular.x = 0.0
     msg.angular.y = 0.0
-    msg.angular.z = 2.5
+    msg.angular.z = self.velocity*5
 
-    msg2 = Twist()
-    msg2.linear.x = self.vel
-    msg2.linear.y = 0.0
-    msg2.linear.z = 0.0
-    msg2.angular.x = 0.0
-    msg2.angular.y = 0.0
-    msg2.angular.z = 2.5
-
-    self.pub.publish(msg)
-    self.pub.publish(msg2)
-    self.get_logger().info(f'Seding message: [{msg}]')
-    self.vel += 0.04
-    if self.vel > 3.0:
-      self.vel = 0.0
+    self.move_turtle.publish(msg)
+    self.get_logger().info(f'Published message: {msg.linear}, {msg.angular}')
+    self.velocity += 0.08
+    if self.velocity > 2:
+      self.vellocity = 0.0
 
 def main():
   rclpy.init()
@@ -46,4 +37,3 @@ def main():
 
 if __name__ == '__main__':
   main()
-
